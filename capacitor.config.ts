e00cbc550af47291ab68b73ad5c16eb0ebf671b0
Vladekxz <seba020798@hotmail.com>
2023-11-17 09:12:07 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'SebastianMendezApp',
+  appName: 'SMendez',
   webDir: 'www',
   server: {
     androidScheme: 'https'
